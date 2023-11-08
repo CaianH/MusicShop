83382c82742f5f2a6bc3ba09f270f0a44903b0ab
caian <caianhenrique10@gmail.com>
2023-11-07 22:28:54 -0400

    update

--- resources/views/produto.blade.php
+++ resources/views/produto.blade.php
@@ -10,36 +10,41 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="../js/app.js"></script>
 
+</head>
 <body>
-    <form action="/cadastrar-produto" method="POST">
-        @csrf
-
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+<form action="/cadastrar-produto" method="POST">
+    @csrf
+    <div class="row">
+        <div class="col-md-6">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Nome</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="digite o nome..." name="nome">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Marca</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="digite a marca..." name="marca">
+            </div>
         </div>
-
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="digite o nome..." name="nome">
+        <div class="col-md-6">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Modelo</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="digite o modelo" name="modelo">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Descrição</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="descricao"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Preço</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="digite o preço..." name="preco">
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Marca</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="digite a marca..." name="marca">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Modelo</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="digite o modelo" name="modelo">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Descrição</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="descricao"></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Preço</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="digite o preço..." name="preco">
-        </div>
-        <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
+    </div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
 </body>
 </html>
